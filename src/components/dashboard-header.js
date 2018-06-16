a251348e9f2a24734101f9b495f08a4db9f0b29e
Dylan Stratton <dm.stratton94@gmail.com>
2018-06-15 20:48:23 -0500

    header things

--- src/components/dashboard-header.js
+++ src/components/dashboard-header.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import CountDownToDate from './countDownToDate';
-import './dashboard-header.css';
+// import './dashboard-header.css';
 
 const mockData = {
     "trip": {
-         "id": 1,
+        "id": 1,
         "user_id": 1,
         "name": "US Trip",
         "destination": "Los Angeles",
         "description": "fun in the sun",
         "arrival": "2018-09-30T04:00:00.000Z",
         "departure": "2018-10-30T04:00:00.000Z"
-}
+    }
 }
 
 //https://source.unsplash.com/featured/?
 export default function DashboardHeader(props) {
-//console.log(props);
+    //console.log(props);
     return (
         <div className="dash-header">
-                <CountDownToDate givenDate={'08/ 17 / 2018'} />
-                <p className="dash-header_countdown">{mockData.trip.name}</p>
-                <p className="dash-header_destination">{mockData.trip.destination}</p>
+            <CountDownToDate givenDate={'08/ 17 / 2018'} />
+            <p className="dash-header_countdown">{mockData.trip.name}</p>
+            <p className="dash-header_destination">{mockData.trip.destination}</p>
             <div className="dash-header_backgroundimg">
-                <img src="https://source.unsplash.com/featured/?{las vegas}" />
+                <img style={{ height: 300, width: '100%' }} src="https://source.unsplash.com/featured/?las%20vegas" />
             </div>
         </div>
     )
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,39 +4,15 @@ import requiresLogin from './requires-login';
 import DashboardNavigation from './dashboard-navigation';
 import DashboardContent from './dashboard-content';
 
-import { fetchTripData } from '../actions/trip';
-import DashboardHeader from './dashboard-header';
-import Description from './description';
-import Accommodations from './accommodations';
-import Plans from './plans';
-
 export class Dashboard extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(fetchTripData());
-    }
-
     render() {
         return (
             <div className="dashboard">
                 <DashboardNavigation />
                 <DashboardContent />
-                <div className="dashboard-username">
+                {/* <div className="dashboard-username">
                     Username: {this.props.username}
-                </div>
-
-                <DashboardHeader />
-
-                <div className="descriptions">{this.props.closestTrip.trip.description ?
-                    <Description description={this.props.closestTrip.trip.description} />
-                    : ''}</div>
-
-                <div className="accommodations">{this.props.closestTrip.accommodations ?
-                    <Accommodations accommodations={this.props.closestTrip.accommodations} />
-                    : ''}</div>
-                <div className="plans">{this.props.closestTrip.plans ?
-                    <Plans plans={this.props.closestTrip.plans} />
-                    : ''}</div>
-
+                </div> */}
             </div>
         );
     }
@@ -47,7 +23,7 @@ const mapStateToProps = state => {
     return {
         username: state.auth.currentUser.username,
         name: `${currentUser.fullname}`,
-        closestTrip: state.trip.closestTrip
+        // closestTrip: state.trip.closestTrip
     };
 };
 
