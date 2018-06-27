89604cd651de60f2349aeee7770c1fc9e71ef9f8
Dylan Stratton <dm.stratton94@gmail.com>
2018-06-27 09:28:31 -0500

    minor fixes

--- src/components/create-new-accommodations-form.js
+++ src/components/create-new-accommodations-form.js
@@ -7,21 +7,21 @@ import { required, nonEmpty } from "../validators";
 import { nextStep, createNewAccommodation } from "../actions/create-new-trip";
 
 class CreateNewAccommodationsFrom extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      arrivalDate: this.props.arrivalDate || moment().format(),
-      departureDate: this.props.departureDate || moment().format()
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrivalDate: this.props.arrivalDate || moment().format(),
+            departureDate: this.props.departureDate || moment().format()
+        };
+    }
 
-  handleArrivalDateChange(date) {
-    this.setState({ arrivalDate: date.utc().format() });
-  }
+    handleArrivalDateChange(date) {
+        this.setState({ arrivalDate: date.utc().format() });
+    }
 
-  handleDepartureDateChange(date) {
-    this.setState({ departureDate: date.utc().format() });
-  }
+    handleDepartureDateChange(date) {
+        this.setState({ departureDate: date.utc().format() });
+    }
 
     handleSkip() {
         this.props.dispatch(nextStep());
@@ -104,67 +104,7 @@ class CreateNewAccommodationsFrom extends Component {
             </div>
         );
     }
-
-    return (
-      <div className="ct-acc ct-card">
-        <div className="ct-acc__header ct-header">
-          <p className="ct-acc__header--text">Accommodations</p>
-        </div>
-        <form
-          className="ct-acc__form"
-          onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
-        >
-          {error}
-          <label htmlFor="hotel">Hotel / Airbnb</label>
-          <Field component={Input} type="text" name="hotel" id="hotel" />
-          <label htmlFor="address">Address</label>
-          <Field component={Input} type="text" name="address" id="address" />
-          <label htmlFor="accArrivalDate">Arrival Date</label>
-          <DatePicker
-            name="accArrivalDate"
-            selected={moment(this.state.arrivalDate)}
-            onChange={this.handleArrivalDateChange.bind(this)}
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            dateFormat="LLL"
-            placeholderText="Click to select a date"
-          />
-          <label htmlFor="accDepartureDate">Departure Date</label>
-          <DatePicker
-            name="accDepartureDate"
-            selected={moment(this.state.departureDate)}
-            onChange={this.handleDepartureDateChange.bind(this)}
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            dateFormat="LLL"
-            placeholderText="Click to select a date"
-          />
-          <label htmlFor="phoneNumber">Phone Number</label>
-          <Field
-            component={Input}
-            type="text"
-            name="phoneNumber"
-            id="phoneNumber"
-          />
-          <div className="ct-next-skip">
-            <button type="button" className="ct-buildGroup__skip skip">
-              Skip
-            </button>
-            <button
-              type="submit"
-              className="ct-buildGroup__next next"
-              disabled={this.props.pristine || this.props.submitting}
-            >
-              Next
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
 }
 export default reduxForm({
-  form: "createNewAccommodationsFrom"
+    form: "createNewAccommodationsFrom"
 })(CreateNewAccommodationsFrom);
