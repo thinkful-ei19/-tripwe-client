0b1c777f1c562880d3eb3a41f0e98b8aefc5c592
biancavee15 <vasquezbmarie@gmail.com>
2018-06-28 17:39:58 -0400

    added heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+  process.env.REACT_APP_API_BASE_URL || "https://tripwe-server.herokuapp.com/api";
 export const CLIENT_ORIGIN =
   process.env.CLIENT_ORIGIN || "http://localhost:3000";
 export const MAPS_API_KEY = process.env.REACT_APP_MAPS_API_KEY;
