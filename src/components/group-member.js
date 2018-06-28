7dfea1f01bf5d6dc92ab0e08ce6022dd7236d758
Dylan Stratton <dm.stratton94@gmail.com>
2018-06-28 14:11:05 -0500

    fix budget styling

--- src/components/group-member.js
+++ src/components/group-member.js
@@ -10,80 +10,81 @@ class GroupMember extends Component {
   }
 
   handleInfo() {
-    this.setState({ showInfo: true });
+    this.setState({ showInfo: !this.state.showInfo });
   }
 
   render() {
     console.log(this.props, "STATE")
-  return (
-    <div className="g-member">
-      <div className="g-member__main">
-        <i className="fas fa-user-circle g-member__main--icon" />
-        <div className="g-member__main--sub">
-          <p className="g-member__name">{this.props.fullname}</p>
-          <p className="g-member__flight-info">
-            <span className="g-member__dep--airport">
-              {this.props.incomingdepartureairport}
-            </span>
-            <i className="fas fa-arrow-right g-member__dep--icon"/>
-            <span className="g-member__arr--airport">
-              {this.props.incomingarrivalairport}
-            </span>
-            <span className="g-member__flight-num">
-              Flight {this.props.incomingflightnum}
-            </span>
-            {this.state.showInfo ?
-              (
-              <span className="g-member__arr--time">
-                departing at &ensp;
+    return (
+      <div className="g-member">
+        <div className="g-member__main">
+          <i className="fas fa-user-circle g-member__main--icon" />
+          <div className="g-member__main--sub">
+            <p className="g-member__name">{this.props.fullname}</p>
+            <p className="g-member__flight-info">
+              <span className="g-member__dep--airport">
+                {this.props.incomingdepartureairport}
+              </span>
+              <i className="fas fa-arrow-right g-member__dep--icon" />
+              <span className="g-member__arr--airport">
+                {this.props.incomingarrivalairport}
+              </span>
+              <span className="g-member__flight-num">
+                Flight {this.props.incomingflightnum}
+              </span>
+              {this.state.showInfo ?
+                (
+                  <span className="g-member__arr--time">
+                    departing at &ensp;
                 <Moment format="MM/DD/YYYY HH:mm">
-                  {this.props.incomingdeparturetime}
+                      {this.props.incomingdeparturetime}
+                    </Moment>
+                  </span>
+                ) : null}
+              <span className="g-member__arr--time">
+                arriving on &ensp;
+              <Moment format="MM/DD/YYYY HH:mm">
+                  {this.props.incomingarrivaltime}
                 </Moment>
               </span>
-              ) : null }
-            <span className="g-member__arr--time">
-              arriving on &ensp;
-              <Moment format="MM/DD/YYYY HH:mm">
-                {this.props.incomingarrivaltime}
-              </Moment>
-            </span>
-          </p>
+            </p>
+          </div>
         </div>
+        {this.state.showInfo ? (
+          <div className="g-member__main--sub g-member__main--sub-details">
+            <p className="g-member__flight-info">
+              <span className="g-member__dep--airport">
+                {this.props.outgoingdepartureairport}
+              </span>
+              <i className="fas fa-arrow-right g-member__dep--icon" />
+              <span className="g-member__arr--airport">
+                {this.props.outgoingarrivalairport}
+              </span>
+              <span className="g-member__flight-num">
+                Flight {this.props.outgoingflightnum}
+              </span>
+              <span className="g-member__arr--time">
+                departing at &ensp;
+            <Moment format="MM/DD/YYYY HH:mm">
+                  {this.props.outgoingarrivaltime}
+                </Moment>
+              </span>
+              <span className="g-member__arr--time">
+                arriving on &ensp;
+            <Moment format="MM/DD/YYYY HH:mm">
+                  {this.props.outgoingdeparturetime}
+                </Moment>
+              </span>
+            </p>
+          </div>)
+          : null}
+        <i
+          onClick={() => this.handleInfo()}
+          className="fas fa-info-circle g-member__info--icon"
+        />
       </div>
-      {this.state.showInfo ? (
-      <div className="g-member__main--sub">
-        <p className="g-member__flight-info">
-          <span className="g-member__dep--airport">
-            {this.props.outgoingdepartureairport}
-          </span>
-          <i className="fas fa-arrow-right g-member__dep--icon"/>
-          <span className="g-member__arr--airport">
-            {this.props.outgoingarrivalairport}
-          </span>
-          <span className="g-member__flight-num">
-            Flight {this.props.outgoingflightnum}
-          </span>
-          <span className="g-member__arr--time">
-            departing at &ensp;
-            <Moment format="MM/DD/YYYY HH:mm">
-              {this.props.outgoingarrivaltime}
-            </Moment>
-          </span>
-          <span className="g-member__arr--time">
-            arriving on &ensp;
-            <Moment format="MM/DD/YYYY HH:mm">
-              {this.props.outgoingdeparturetime}
-            </Moment>
-          </span>
-        </p>
-      </div> )
-      : null }
-      <i
-        onClick={() => this.handleInfo()}
-        className="fas fa-info-circle g-member__info--icon"
-      />
-    </div>
-  )};
+    )
+  };
 };
 
 export default GroupMember;
